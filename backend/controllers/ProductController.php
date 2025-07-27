bd7e51a5a9327139ca38e7b09fba4776db5f5bdd
Modi97 <102935099+JoseModi97@users.noreply.github.com>
2025-07-27 20:36:20 +0300

    New files

--- backend/controllers/ProductController.php
+++ backend/controllers/ProductController.php
@@ -30,7 +30,47 @@ class ProductController extends Controller
         ];
     }
 
+    public function actionIndex()
+    {
+        $productsDataProvider = new ActiveDataProvider([
+            'query' => Product::find(),
+        ]);
+        $categories = Category::find()->all();
 
+        return $this->render('index', [
+            'productsDataProvider' => $productsDataProvider,
+            'categories' => $categories,
+        ]);
+    }
+
+    public function actionList()
+    {
+        $dataProvider = new ActiveDataProvider([
+            'query' => Product::find(),
+        ]);
+
+        return $this->render('list', [
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    public function actionGetProducts()
+    {
+        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $products = Product::find()
+            ->select('products.*, categories.name as category_name')
+            ->leftJoin('categories', 'products.category_id = categories.id')
+            ->asArray()
+            ->all();
+        return $products;
+    }
+
+    public function actionGetCategories()
+    {
+        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $categories = Category::find()->asArray()->all();
+        return $categories;
+    }
 
     public function actionCreate()
     {
